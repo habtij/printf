5bd989a94830840fd940d7c06f5dfba87ec7368f
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-23 06:09:31 -0800

    task 0

---
+++ test/0-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char c = 'a';
+	char *s = "Hello World!";
+	int len, len1;
+
+	len = _printf("This is a char c: %c%%\n", c);
+	len1 = printf("This is a char c: %c%%\n", c);
+	printf("%d\n", len);
+	printf("%d\n", len1);
+	len = _printf("This is a string s: %s\n", s);
+	len1 = printf("This is a string s: %s\n", s);
+	printf("%d\n", len);
+	printf("%d\n", len1);
+	return (0);
+}
